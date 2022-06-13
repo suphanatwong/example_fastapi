ac6306ea59f780045e299a3a2e620a6d8825f1db
Suphanat Wongsanuphat <suphanatwongsanuphat@Suphanats-MacBook-Pro.local>
2022-06-13 16:14:38 +0700

    added procfile

--- alebmic/versions/e200e5d7b0a0_add_fk_to_post.py
+++ alebmic/versions/e200e5d7b0a0_add_fk_to_post.py
@@ -21,7 +21,7 @@ def upgrade():
     op.add_column('posts',sa.Column('owner_id', sa.Integer(),nullable =False))
     op.create_foreign_key('post_users_fk', source_table="posts", referent_table="users",
     local_cols=[
-                    'owner_id'],remote_cols=['id'],ondelete="CASCADE")
+                     'owner_id'],remote_cols=['id'],ondelete="CASCADE")
 
     pass
 
