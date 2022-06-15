df108c7f58f363dfce169a3443aa8c9af0f6c2bd
suphanatwong <107363909+suphanatwong@users.noreply.github.com>
2022-06-15 10:39:31 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     database_username: str
     secret_key: str
     algorithm: str
-    access_token_expire_minute: int
+    access_token_expire_minutes: int
 
     class Config:
         env_file= ".env"
