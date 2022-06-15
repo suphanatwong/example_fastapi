eb2c071b6dd3701d828677c575713f48736a9d51
Suphanat Wongsanuphat <suphanatwongsanuphat@Suphanats-MacBook-Pro.local>
2022-06-15 10:46:18 +0700

    edit db

--- app/database.py
+++ app/database.py
@@ -27,7 +27,7 @@ def get_db():
     finally: 
         db.close()
 
-# while True: 
+ while True: 
 
 #     try:
 #         conn=psycopg2.connect(host ='localhost', database = 'fastapi', user = 'postgres', 
